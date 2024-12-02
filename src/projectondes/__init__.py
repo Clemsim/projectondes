87658e0973979cd607d040899617bf693f3d14d7
Ghastrod <clement.jallat@gmail.com>
2024-12-02 09:18:09 +0100

    Tout marche, même matplotlib

--- src/projectondes/__init__.py
+++ src/projectondes/__init__.py
@@ -1,3 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 def main() -> int:
     print("Hello from projectondes!")
+    plt.plot(np.sin(np.linspace(0, 2*np.pi, 100)))
+    plt.show()
     return 0
--- src/projectondes/__main__.py
+++ src/projectondes/__main__.py
@@ -1,4 +1,7 @@
 import projectondes
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 sys.exit(projectondes.main())
